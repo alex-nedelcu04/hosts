6355d8b6663b6678739ce89ae86aa74e7b9c6ed5
Alex Nedelcu <alexned2004@gmail.com>
2024-11-16 14:18:46 +0200

    Al doilea commit: varianta modificata a scriptului

--- citire_ip.sh
+++ citire_ip.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+dns=$1
+
+validare_ip() {
+   local name=$1
+   local ip=$2
+   local dns=$3
+
+   ip_nslookup=$(nslookup "$name" "$dns"  | grep 'Address:' | head -n 2 | tail -n 1 | cut -d ' ' -f 2)
+
+   if [[ "$ip_nslookup" != "$ip" ]]; then
+        echo "Bogus IP for $name in /etc/hosts!"
+   fi
+
+}
+
 cat /etc/hosts | while read -r  ip name
 do
     if [[ "$ip" == \#* ]]; then
 	break
     fi
-	
-    if [ "$name" == "localhost" ] ||  [ "$name" == "FMI" ]; then
-	ip_nslookup=$(nslookup "$name"  | grep 'Address:' | head -n 2 | tail -n 1 | cut -d ' ' -f 2)
-    else
-    	ip_nslookup=$(nslookup "$name" 8.8.8.8 | grep 'Address:' | head -n 2 | tail -n 1 | cut -d ' ' -f 2)
-    fi
-
-    if [[ "$ip_nslookup" != "$ip" ]]; then
-	echo "Bogus IP for $name in /etc/hosts!"
-    fi
+    
+    validare_ip "$name" "$ip" "$dns"
 done
